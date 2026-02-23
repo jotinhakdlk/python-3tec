e870650c322947fefc9ab3a8f8cec68dafaeab54
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-02-23 11:50:39 -0300

    Add files via upload

--- adivinhar python/main.py
+++ adivinhar python/main.py
@@ -1,17 +1,22 @@
+import random as rd
 print("_________________________________")
 print("Seja bem vindo ao adivinha python!")
 print("_________________________________")
 
-n_secreto = 50
+n_secreto = rd.randrange(1, 101)
 n_tentativas = 5
 rodada = 1
-
+print(n_secreto)
 for n_tentativas in range(1, n_tentativas + 1):
     print(f"rodada {rodada}/{n_tentativas}")
-    entrada = int(input("digite um valor aleatório: "))
+    entrada = int(input("digite um número entre 1 e 100: "))
     acerto = entrada == n_secreto
     chutemaior = entrada > n_secreto
     chutemenor = entrada < n_secreto
+
+    if(entrada > 100 or entrada < 1):
+        print("Tu digitou um valor inválido doido")
+        continue
 
     print(f"Você digitou o número: {entrada}")
 
