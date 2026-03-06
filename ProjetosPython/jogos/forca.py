c1e3e04e4e3a4054ffc212eeb17d651b28a7d6c5
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-03-06 11:47:44 -0300

    Add files via upload

---
+++ ProjetosPython/jogos/forca.py
@@ -0,0 +1,4 @@
+def jogar_forca():
+    print("__________________________")
+    print("Seja bem vindo ao forca!")
+    print("__________________________")
---
+++ Trabalho ADS 3tec/main.py
@@ -0,0 +1,13 @@
+coletadado = open('dados_cliente.txt', 'a')
+
+print("Olá! Gostaria de coletar seus dados inutilmente")
+
+
+print("Qual seu nome?")
+Nome = input("Resposta: ")
+
+print("Quantos anos você tem?")
+idade = input("Resposta: ")
+
+print("Qual seu número de telefone?")
+telefone = input("Resposta: ")
