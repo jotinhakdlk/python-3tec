6ff12f4b68ccba592744cbf7ee5cee91d0b53ad1
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-05-29 12:27:46 -0300

    Add files via upload

--- python orientado a objeto/conta.py
+++ python orientado a objeto/conta.py
@@ -1,25 +1,60 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     ## Declaração dos métodos (funções)
     def extrato(self):
-        print(f"Saldo {self.saldo} do titular {self.titular}")
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
     
     def depositar(self, valor):
         if(valor < 0):
             print("Não é possível depositar um valor negativo")
         else:
-            self.saldo += valor
+            self.__saldo += valor
 
     def sacar(self, valor):
-        if(self.saldo < valor):
+        if(self.__saldo < valor):
             print(f"Não é possível sacar este valor ({valor})")
         else:
-            self.saldo -= valor
+            self.__saldo -= valor
 
     def extrato(self):
-        print(f'Seu saldo é {self.saldo}')
+        print(f'Seu saldo é {self.__saldo}')
+
+    def transferir(self, valor, destino):
+        if(self.__saldo < valor) or (valor < 0):
+            print("Não é possível realizar a transferência")
+        else:
+            self.sacar(valor)
+            destino.depositar(valor)
+    
+## Método para retornar apenas valores das propriedades    
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def limite(self):
+        return self.__limite
+
+## Métodos para manipular os valores das propriedades
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @saldo.setter
+    def saldo(self, saldo):
+        self.__saldo = saldo
+    
--- python orientado a objeto/exec.py
+++ python orientado a objeto/exec.py
@@ -1,7 +1,6 @@
 from conta import Conta
 
 conta = Conta(321, "João", 55.0, 5000.0)
-conta2 = Conta(555, "Fulano", 100.0, 1000.0)
+conta1 = Conta(555, "Fulano", 100.0, 1000.0)
 
-conta.sacar(100)
-conta.extrato()
+print(conta.titular)
